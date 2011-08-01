731fd3dbfa961ae973cd52997ae4edf7b40a72d4
Frank Becker <fb@alien8.de>
2011-08-01 21:04:24 +0200

    Determine Python version, determine virtualenv path
        - Python version is determined, previously was hard coded to 2.7
        - Path to created virtualenv is taken from $VIRTUAL_ENV, previously hard coded to $HOME/Envs

--- bin/init.sh
+++ bin/init.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
+
+## Settings
+SCM=hg # SCM of the final project. Tested for 'git' and 'hg'
+PYTHONVERSION=$(/usr/bin/env python --version 2>&1 | \
+    sed 's/^Python\ \([0-9]\.[0-9]\).*$/\1/')
+
+
+# main()
 echo "Setting up a new django-cms development environment."
 echo "Answer some questions before running the installation."
 
-echo "What is the path to your virtualenvwrapper.sh?"
-echo "Standard is: /usr/local/bin/virtualenvwrapper.sh"
-echo "Leave empty to use standard: "
-read venvwrappersh
-if [ "$venvwrappersh" == "" ]
+echo -ne "\nWhat is the path to your virtualenvwrapper.sh?\n"
+venvwrappersh=$(which virtualenvwrapper.sh)
+[ $? -ne 0 ] && venvwrappersh="not found"
+
+echo "Found it on: $venvwrappersh"
+echo "Leave empty to use the found one: "
+read venvwrappersh_input
+if [ "x$venvwrappersh_input" != "x" ]
   then
-    venvwrappersh="/usr/local/bin/virtualenvwrapper.sh"
+    venvwrappersh=$venvwrappersh_input
 fi
 if [ ! -f $venvwrappersh ]
   then
@@ -16,12 +27,17 @@ if [ ! -f $venvwrappersh ]
     exit 1
 fi
 
-echo "Virtualenv name:"
+echo "Enter a virtualenv name (required):"
 read virtualenvname
+if [ "x$virtualenvname" == "x" ]
+  then
+    echo -ne "\nThe name of the virtualenv cannot be empty!\nExiting.\n"
+    exit 1
+fi
 
 source $venvwrappersh
 echo "Installing all needed modules into a virtualenv"
-mkvirtualenv -p python2.7 $virtualenvname
+mkvirtualenv -p python$PYTHONVERSION $virtualenvname
 workon $virtualenvname
 pip install -r bin/requirements.txt
 
@@ -29,10 +45,9 @@ echo "Updating git submodules..."
 git submodule update --init --recursive
 
 echo "Creating static folders..."
-mkdir ./webapps/static
-mkdir ./webapps/static/img
-mkdir ./webapps/static/css
-mkdir ./webapps/static/js
+for folder in "./webapps/static ./webapps/static/img ./webapps/static/css ./webapps/static/js"; do
+    mkdir $folder
+done
 touch ./webapps/static/css/styles.sass
 
 echo "Copying the parts of html5-boilerplate that we need..."
@@ -65,12 +80,12 @@ echo "Creating symlinks..."
 mkdir -p ./webapps/media
 cd ./webapps/media
 echo "What is the name of your virtualenv: "
-ln -s $HOME/Envs/$virtualenvname/lib/python2.7/site-packages/cms/media/cms
-ln -s $HOME/Envs/$virtualenvname/lib/python2.7/site-packages/filer/media/filer
+ln -s $VIRTUAL_ENV/lib/python$PYTHONVERSION/site-packages/cms/media/cms
+ln -s $VIRTUAL_ENV/lib/python$PYTHONVERSION/site-packages/filer/media/filer
 cd ../..
 
 echo "Creating local_settings.py ..."
-cd webapps/django/project/
+cd ./webapps/django/project/
 cp local_settings.py.sample local_settings.py
 cd ../../..
 sed -i s@projectroot@$(pwd)/@g webapps/django/project/local_settings.py
@@ -80,11 +95,12 @@ cp ./lib/.gitignore .
 rm -rf ./lib/
 
 echo "Initiate a new git project..."
-git init
-git add .
-git commit -m "Initial Commit"
+$SCM init
+$SCM add .
+$SCM commit -m "Initial Commit"
 
-workon $virtualenvname
+# FIXME a8 next line not required
+# workon $virtualenvname
 cd webapps/django/project
 ./manage.py syncdb --migrate
 ./manage.py collectstatic

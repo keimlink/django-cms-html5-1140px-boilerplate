910a85bc602a6932784a4ab1c46c8aedc6e0f88a
Frank Becker <fb@alien8.de>
2011-08-08 12:45:10 +0200

    more refactoring of installer
    - Fixed a few bugs   - Mac OS X compatible   - more refactoring (adding
    functions)

--- bin/init.sh
+++ bin/init.sh
@@ -6,9 +6,12 @@ PYTHONVERSION=$(/usr/bin/env python --version 2>&1 | \
     sed 's/^Python\ \([0-9]\.[0-9]\).*$/\1/')
 PIP_REQUIREMENTS='config/requirements-devel.txt'
 APP_TEMPLATE="webapp-templates"
-APP_DESTINATION="project"
+APP_DESTINATION="webapps"
 
 # Nothing has to be changed below this line.
+
+## Set up
+VIRTUAL_ENV=""  # clean variable, will be set on 'workon virtualenvname'
 
 ## Functions
 function set_django_secret_key () {
@@ -16,8 +19,9 @@ function set_django_secret_key () {
 MATRIX="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+=?></\-"
     KEY_LENGTH="50"
     while [ "${n:=1}" -le "$KEY_LENGTH" ]; do
-        DJANGO_SECRET_KEY="$PASS${MATRIX:$(($RANDOM%${#MATRIX})):1}"
-        let n+=1
+        DJANGO_SECRET_KEY="$DJANGO_SECRET_KEY${MATRIX:$(($RANDOM%${#MATRIX})):1}"
+        n=$(($n+1))
+        #let n+=1
     done
 }
 
@@ -87,6 +91,69 @@ function copy_1140px () {
     done
 }
 
+function split_up_html5_boilerplate_css () {
+    echo "Splitting up html5-boilerplate css..."
+    SPLIT=$(grep -n "/* Primary styles" "$APP_DESTINATION"/static/css/style.css | awk -F":" '{ print $1 }')
+    #SPLITHEAD=`expr $SPLIT - 1`
+    SPLITHEAD=$(($SPLIT-1))
+    SPLITTAIL=$(($SPLIT+3))
+    head -n $SPLITHEAD "$APP_DESTINATION"/static/css/style.css > "$APP_DESTINATION"/static/css/boilerplate.css
+    tail -n +$SPLITTAIL "$APP_DESTINATION"/static/css/style.css > "$APP_DESTINATION"/static/css/boilerplate_media.css
+}
+
+function cleanup_installer () {
+    echo "Removing all the installer parts ..."
+    rm -rf .git
+    rm .gitignore
+    rm .gitmodules
+    rm README.rst
+    rm "$APP_DESTINATION"/static/css/style.css
+    rm -rf "$APP_TEMPLATE"
+
+    echo "Remove lib folder..."
+    cp ./lib/.gitignore .
+    rm -rf ./lib/
+}
+
+function symlink_modules_media () {
+    echo "Creating symlinks..."
+    mkdir -p "$APP_DESTINATION"/media
+    cd "$APP_DESTINATION"/media
+    [ x"$VIRTUAL_ENV" = "x" ] && { echo -ne "\nSomething strange happend. No virtualenv active. Exit.\n"; exit 1; }
+    if [ ! -L cms ]; then
+        # Need to find real place of module. Installing from repo will just create an egg link
+        CMS_PATH=$(python -c "import os, cms; print os.path.dirname(os.path.abspath(cms.__file__))")
+        echo $CMS_PATH
+        # try to find the media link. Changed w/ 2.2 using django.contrib.staticfiles
+        if [ -d "$CMS_PATH"/media/cms ]; then
+            ln -s "$CMS_PATH"/media/cms .
+        elif [ -d "$CMS_PATH"/static/cms ]; then
+            # link it for backward compatibility
+            ln -s "$CMS_PATH"/static/cms .
+        else
+            echo "django-cms media/static files not found. Probably, that's bad."
+        fi
+    else
+        echo "Sym link to cms/media/cms alredy exists in $APP_DESTINATION/media. I do not overwrite it."
+    fi
+    if [ ! -L filer ]; then
+        ln -s $(python -c "import os, filer; print os.path.dirname(os.path.abspath(filer.__file__))")/media/filer .
+    else
+        echo "Sym link to filer/media/filer alredy exists in $APP_DESTINATION/media. I do not overwrite it."
+    fi
+    cd ../..
+}
+
+function set_local_settings () {
+    echo "Adjusting local_settings.py ..."
+    set_django_secret_key
+    cd "$APP_DESTINATION"/django/project/
+    cp local_settings.py.sample local_settings.py
+    sed -i '' "s#projectroot#$(pwd)/#" local_settings.py
+    sed -i ''  "s/^SECRET_KEY\ =.*$/SECRET_KEY\ =\ \"${DJANGO_SECRET_KEY}\"/" local_settings.py
+    cd ../../..
+}
+
 # main()
 echo "Setting up a new django-cms development environment."
 echo "Answer some questions before running the installation."
@@ -104,11 +171,11 @@ fi
 workon $VIRTUALENVNAME
 
 echo "Installing all needed modules into a virtualenv"
-pip install -r $PIP_REQUIREMENTS
-[ $? -ne 0 ] && { echo -ne "\nProblem while installing dependencies via pip!\nExit.\n"; exit 1; }
+#pip install -r $PIP_REQUIREMENTS
+#[ $? -ne 0 ] && { echo -ne "\nProblem while installing dependencies via pip!\nExit.\n"; exit 1; }
 
 echo "Updating git submodules..."
-git submodule update --init --recursive
+#git submodule update --init --recursive
 
 echo "Creating Project folder: $APP_DESTINATION"
 [ -d "$APP_DESTINATION" ] || mkdir $APP_DESTINATION
@@ -117,42 +184,13 @@ cp -r "$APP_TEMPLATE"/* "$APP_DESTINATION"/
 create_static_folders
 copy_html5-boilerplate
 copy_1140px
+split_up_html5_boilerplate_css
+symlink_modules_media
+set_local_settings
 
 exit 0
 
-echo "Splitting up html5-boilerplate css..."
-SPLIT=`grep -n "/* Primary Styles" "$APP_DESTINATION"/static/css/style.css | awk -F":" '{ print $1 }'`
-SPLITHEAD=`expr $SPLIT - 1`
-SPLITTAIL=`expr $SPLIT + 3`
-head --lines=$SPLITHEAD "$APP_DESTINATION"/static/css/style.css > "$APP_DESTINATION"/static/css/boilerplate.css
-tail -n +$SPLITTAIL "$APP_DESTINATION"/static/css/style.css > "$APP_DESTINATION"/static/css/boilerplate_media.css
-
-echo "Removing the parts we dont want..."
-rm -rf .git
-rm .gitignore
-rm .gitmodules
-rm README.rst
-rm "$APP_DESTINATION"/static/css/style.css
-
-echo "Creating symlinks..."
-mkdir -p "$APP_DESTINATION"/media
-cd "$APP_DESTINATION"/media
-echo "What is the name of your virtualenv: "
-ln -s $VIRTUAL_ENV/lib/python$PYTHONVERSION/site-packages/cms/media/cms
-ln -s $VIRTUAL_ENV/lib/python$PYTHONVERSION/site-packages/filer/media/filer
-cd ../..
-
-echo "Creating local_settings.py ..."
-set_django_secret_key
-cd "$APP_DESTINATION"/django/project/
-cp local_settings.py.sample local_settings.py
-cd ../../..
-sed -i s@projectroot@$(pwd)/@g webapps/django/project/local_settings.py
-sed -i "s/^SECRET_KEY=.*$/SECRET_KEY='${DJANGO_SECRET_KEY}'/" webapps/django/project/local_settings.py
-
-echo "Remove lib folder..."
-cp ./lib/.gitignore .
-rm -rf ./lib/
+#cleanup_installer
 
 echo "Initiate a new scm repository..."
 $SCM init
